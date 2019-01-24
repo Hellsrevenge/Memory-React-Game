7abd1cd6cbcb022e388cf06f6a57ab12b7aa9c08
Hellsrevenge <ivark26@gmail.com>
2019-01-23 23:04:19 -0800

    Updates

---
+++ precache-manifest.cca33e624655205d947eca9c56aee1b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f9c4234422262ce60d1",
+    "url": "/Memory-React-Game/static/css/main.c25f9c94.chunk.css"
+  },
+  {
+    "revision": "7f9c4234422262ce60d1",
+    "url": "/Memory-React-Game/static/js/main.7f9c4234.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/Memory-React-Game/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "aa3367e31427fe804794",
+    "url": "/Memory-React-Game/static/js/runtime~main.aa3367e3.js"
+  },
+  {
+    "revision": "529388839d0accfbc6e55e09a06cbbfa",
+    "url": "/Memory-React-Game/index.html"
+  }
+];
